8d06e103e0405f54f26d98b9809381398f1aeccd
Nikhil <iamnikkii21@gmail.com>
2024-06-04 18:44:59 +0530

    Done

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:money_exchange/view/home_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: HomeScreen(),
+    );
+  }
+}
